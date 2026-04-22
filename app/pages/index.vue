719f77ff14390a99368bf22ee160fbd4ecd76b25
vlmna13 <vlmna13@gmail.com>
2026-04-22 12:56:15 +0300

    feat: add universe card component and selection page

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { universes } from '~/data/universes';
+</script>
+
+<template>
+  <div class="flex gap-8 p-8">
+    <UniverseCard v-for="u in universes" :key="u.id" :universe="u" />
+  </div>
+</template>
