a49905e3ae486369e196625228c2d4c0045fabc5
vlmna13 <vlmna13@gmail.com>
2026-04-22 14:11:08 +0300

    feat: add star background component

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
+    <StarBackground />
     <NuxtPage />
   </div>
 </template>
+
+<style>
+body {
+  width: 100%;
+  min-height: 100vh;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
